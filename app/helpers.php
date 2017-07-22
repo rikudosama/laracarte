48e894e8fc97bb2c822bf97bda604f4a8cb65988
lengam bonaveneture <bjean633@gmail.com>
2017-07-22 03:19:01 +0000

    add some unit test for page_title helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,8 @@
-<?php  
+<?php
 
 if (! function_exists('page_title')) {
 	function page_title($title){
-		$base_title = config('app.name').' - list of artisans';
+		$base_title = config('app.name').' - List of artisans';
 		if ($title === '') {
 			return $base_title;
 		}else{
